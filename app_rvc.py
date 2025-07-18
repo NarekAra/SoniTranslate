bcd03a1b79172c159ceb405c58130c4741787b69
narek <narek.arakelyan@outlook.be>
2025-07-18 19:02:49 +0200

    formatting

--- app_rvc.py
+++ app_rvc.py
@@ -1,63 +1,101 @@
-import argparse
-import copy
-import hashlib
-import json
-import logging
-import os
-import sys
-import time
-
 import gradio as gr
-import torch
+from soni_translate.logging_setup import (
+    logger,
+    set_logging_level,
+    configure_logging_libs,
+); configure_logging_libs() # noqa
 import whisperx
-from pydub import AudioSegment
-
+import torch
+import os
 from soni_translate.audio_segments import create_translated_audio
-from soni_translate.language_configuration import (BARK_VOICES_LIST, LANGUAGES,
-                                                   LANGUAGES_LIST,
-                                                   OPENAI_TTS_MODELS,
-                                                   UNIDIRECTIONAL_L_LIST,
-                                                   VITS_VOICES_LIST)
-from soni_translate.languages_gui import language_data, news
-from soni_translate.logging_setup import (;, configure_logging_libs,  # noqa
-                                          logger, set_logging_level)
-from soni_translate.mdx_net import (MDX_DOWNLOAD_LINK, UVR_MODELS,
-                                    mdxnet_models_dir)
-from soni_translate.postprocessor import (DOCS_OUTPUT_TYPE_OPTIONS,
-                                          OUTPUT_TYPE_OPTIONS,
-                                          get_no_ext_filename,
-                                          get_subtitle_speaker, media_out,
-                                          sound_separate)
-from soni_translate.preprocessor import (audio_preprocessor,
-                                         audio_video_preprocessor)
-from soni_translate.speech_segmentation import (ASR_MODEL_OPTIONS,
-                                                COMPUTE_TYPE_CPU,
-                                                COMPUTE_TYPE_GPU, align_speech,
-                                                diarization_models,
-                                                diarize_speech,
-                                                find_whisper_models,
-                                                transcribe_speech)
-from soni_translate.text_multiformat_processor import (
-    BORDER_COLORS, break_aling_segments, create_video_from_images,
-    determine_chunk_size, doc_to_txtximg_pages, document_preprocessor,
-    fix_timestamps_docs, linguistic_level_segments, merge_video_and_audio,
-    page_data_to_segments, plain_text_to_segments, process_subtitles,
-    segments_to_plain_text, srt_file_to_segments, update_page_data)
-from soni_translate.text_to_speech import (accelerate_segments,
-                                           audio_segmentation_to_voice,
-                                           coqui_xtts_voices_list,
-                                           create_wav_file_vc,
-                                           edge_tts_voices_list,
-                                           piper_tts_voices_list)
+from soni_translate.text_to_speech import (
+    audio_segmentation_to_voice,
+    edge_tts_voices_list,
+    coqui_xtts_voices_list,
+    piper_tts_voices_list,
+    create_wav_file_vc,
+    accelerate_segments,
+)
 from soni_translate.translate_segments import (
-    DOCS_TRANSLATION_PROCESS_OPTIONS, TRANSLATION_PROCESS_OPTIONS,
-    translate_text)
-from soni_translate.utils import (copy_files, download_list, download_manager,
-                                  get_link_list, get_valid_files,
-                                  is_audio_file, is_subtitle_file,
-                                  remove_directory_contents, remove_files,
-                                  run_command, upload_model_list)
+    translate_text,
+    TRANSLATION_PROCESS_OPTIONS,
+    DOCS_TRANSLATION_PROCESS_OPTIONS
+)
+from soni_translate.preprocessor import (
+    audio_video_preprocessor,
+    audio_preprocessor,
+)
+from soni_translate.postprocessor import (
+    OUTPUT_TYPE_OPTIONS,
+    DOCS_OUTPUT_TYPE_OPTIONS,
+    sound_separate,
+    get_no_ext_filename,
+    media_out,
+    get_subtitle_speaker,
+)
+from soni_translate.language_configuration import (
+    LANGUAGES,
+    UNIDIRECTIONAL_L_LIST,
+    LANGUAGES_LIST,
+    BARK_VOICES_LIST,
+    VITS_VOICES_LIST,
+    OPENAI_TTS_MODELS,
+)
+from soni_translate.utils import (
+    remove_files,
+    download_list,
+    upload_model_list,
+    download_manager,
+    run_command,
+    is_audio_file,
+    is_subtitle_file,
+    copy_files,
+    get_valid_files,
+    get_link_list,
+    remove_directory_contents,
+)
+from soni_translate.mdx_net import (
+    UVR_MODELS,
+    MDX_DOWNLOAD_LINK,
+    mdxnet_models_dir,
+)
+from soni_translate.speech_segmentation import (
+    ASR_MODEL_OPTIONS,
+    COMPUTE_TYPE_GPU,
+    COMPUTE_TYPE_CPU,
+    find_whisper_models,
+    transcribe_speech,
+    align_speech,
+    diarize_speech,
+    diarization_models,
+)
+from soni_translate.text_multiformat_processor import (
+    BORDER_COLORS,
+    srt_file_to_segments,
+    document_preprocessor,
+    determine_chunk_size,
+    plain_text_to_segments,
+    segments_to_plain_text,
+    process_subtitles,
+    linguistic_level_segments,
+    break_aling_segments,
+    doc_to_txtximg_pages,
+    page_data_to_segments,
+    update_page_data,
+    fix_timestamps_docs,
+    create_video_from_images,
+    merge_video_and_audio,
+)
+from soni_translate.languages_gui import language_data, news
+import copy
+import logging
+import json
+from pydub import AudioSegment
 from voice_main import ClassVoices
+import argparse
+import time
+import hashlib
+import sys
 
 directories = [
     "downloads",
@@ -76,7 +114,6 @@ directories = [
 ]
 
 import dotenv
-
 dotenv.load_dotenv()
 
 
